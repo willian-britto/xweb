888a6d2f966ea7ac6c840a798a12dd48e01fbb12
willian-britto <78366404+willian-britto@users.noreply.github.com>
2021-02-01 20:10:09 +0300

    Update log.c

--- log.c
+++ log.c
@@ -1,4 +1,49 @@
 
+static PyObject* xweb_PY_log_ (const char* const pre, uint preSize, const char* const msg, uint msgSize) {
+
+    if (logFree < (msgSize + 8192)) {
+        logFree = LOG_BUFFER_SIZE;
+        logEnd = logBuffer;
+    }
+
+    memcpy(logEnd, pre, preSize);
+    logEnd += preSize;
+    logFree -= preSize;
+
+    if (thread) {
+        memcpy(logEnd, thread->name, thread->nameSize);
+        logEnd += thread->nameSize;
+        logFree -= thread->nameSize + 1; // JA DESCONTA O \n
+    } elif (class) {
+        memcpy(logEnd, class->name, class->nameSize);
+        logEnd += class->nameSize;
+        logFree -= class->nameSize + 1;
+    } else {
+        memcpy(logEnd, "[main]", 6);
+        logEnd += 6;
+        logFree -= 7;
+    }
+
+    if (msgSize > logFree)
+        msgSize = logFree;
+
+    memcpy(logEnd, msg, msgSize);
+
+    logEnd += msgSize;
+    logFree -= msgSize;
+
+    memcpy(logEnd, "\n", 1);
+
+    logEnd += 1;
+
+#if 1
+    write(STDOUT_FILENO, logBuffer, logEnd - logBuffer);
+    logEnd = logBuffer;
+    logFree = LOG_BUFFER_SIZE;
+#endif
+    return None;
+}
+
 void xweb_log_init (void) {
 
     logBuffer = malloc(LOG_BUFFER_SIZE);
