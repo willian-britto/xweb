aa4ef0c8c85f1a9f5f6fc8b2af00602ebc664ef4
willian-britto <78366404+willian-britto@users.noreply.github.com>
2021-02-01 22:12:26 +0300

    Update thread.h

--- thread.h
+++ thread.h
@@ -1,19 +1,4 @@
 
 typedef struct Thread Thread;
 
-struct Class {
-    Class* next;
-    Class* parentNext; // PRÓXIMA CHILD DO PARENT
-    Class* siteNext; // PRÓXIMA CLASSE DO MESMO SITE
-    Class* parent;
-    Class* childs;
-    Site* site; // CLASSE ROOT DESTA CLASSE
-    Thread* threads; // INSTANCIAS DESTA CLASSE
-    u32 n;
-    u32 nMax;
-    u16 nameSize;
-    u16 reserved;
-    u32 reserved2;
-    char name[];
-};
 
