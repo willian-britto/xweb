1585959f37f7322b9a139b163ca866990276231d
willian-britto <78366404+willian-britto@users.noreply.github.com>
2021-02-01 18:13:29 +0300

    Create proxies.c

---
+++ proxies.c
@@ -0,0 +1,50 @@
+static int xweb_proxy_cmp (const u16* const a, const u16* const b) {
+    return (int)proxiesSortPoints[*a] -
+           (int)proxiesSortPoints[*b];
+}
+
+static void xweb_proxy_add (const uint ip, const uint port, const uint protocol) {
+
+    u64 hash = ((u64)ip << 32) + ((u64)port << 16) + ip + port + protocol;
+
+    u16* ptr = &proxiesRoots[hash & PROXIES_ROOTS_MASK]; hash >>= PROXIES_ROOTS_BITS;
+
+    while (*ptr != PROXY_NONE) { Proxy* const proxy = &proxies[*ptr];
+        if (proxy->ip == ip &&
+            proxy->port == port &&
+            proxy->protocol == protocol)
+            return;
+        ptr = &proxy->childs[hash & PROXIES_CHILDS_MASK]; hash >>= PROXIES_CHILDS_BITS;
+    }
+
+    dbg("ADDING PROXY %u.%u.%u.%u PORT %u PROTOCOL %u", _IP4_ARGS(&ip), port, protocol);
+
+    if (proxiesN != PROXIES_N) { Proxy* const proxy = &proxies[proxiesN];
+
+        proxy->ip        = ip;
+        proxy->port      = port;
+        proxy->protocol  = protocol;
+        proxy->childs[0] = PROXY_NONE;
+        proxy->childs[1] = PROXY_NONE;
+        proxy->childs[2] = PROXY_NONE;
+        proxy->childs[3] = PROXY_NONE;
+
+        for (Site* site = sites; site; site = site->next)
+            site->proxiesPoints[proxiesN] = PROXY_POINTS_ZERO;
+
+        proxiesN++;
+    }
+}
+
+void xweb_proxy_add (const char* const ip_, const uint port, const uint protocol) {
+
+    uint a = 0, b = 0, c = 0, d = 0;
+
+    if (sscanf(ip_, "%u.%u.%u.%u", &a, &b, &c, &d) == 4) {
+
+        const uint ip = IP4(a, b, c, d);
+
+        if (xweb_is_ip_valid_4(ip) && 1 <= port && port <= 0xFFFF && protocol <= 2)
+            xweb_proxy_add(ip, port, protocol);
+    }
+}
