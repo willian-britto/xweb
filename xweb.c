56e540ac7d4ac7fafe6c6713aa6df3684476ca71
willian-britto <78366404+willian-britto@users.noreply.github.com>
2021-02-01 19:46:52 +0300

    Update xweb.c

--- xweb.c
+++ xweb.c
@@ -2716,16 +2716,8 @@ PyMODINIT_FUNC PyInit_xweb (void) {
     xweb_dns_init();
     xweb_proxies_init();
     xweb_hosts_init();
-
-    // INITIALIZE THE SSL LIBRARY
-    wolfSSL_Init();
-
-    // CREATE THE SSL CONTEXT
-    sslCtx = wolfSSL_CTX_new(wolfTLSv1_2_client_method());
-    wolfSSL_CTX_set_verify(sslCtx, SSL_VERIFY_NONE, NULL);
-    wolfSSL_SetIORecv(sslCtx, (CallbackIORecv)xweb_ssl_read);
-    wolfSSL_SetIOSend(sslCtx, (CallbackIOSend)xweb_ssl_write);
-
+    xweb_ssl_init();
+    
     //
     connsN = 0;
     conns = NULL;
